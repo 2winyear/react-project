66d89410eb17b9bc36921544ae821e41030044ea
seungyel <seungyel@seungyelui-MacBookPro.local>
2021-08-09 23:54:36 +0900

    book reading,,

--- book/src/App.js
+++ book/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-const App = props => {
-  return <div>안녕하세요 {props.name}</div>;
-};
+class App extends Component {
+  static defaultProps ={
+    name: '기본이름'
+  };
+  
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+  }
+  render(){
+  const { name, favoriteNumber, children } = this.props; 
+  return( 
+    <div>
+      안녕하세요 {name}
+      제가 좋아하는 숫자는 {favoriteNumber}
+      {children}
+    </div>
+    );
+  }
+}
 
-App.defaultProps ={
-  name: '기본이름'
-};
 
 export default App;
--- book/src/index.js
+++ book/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App/>,
+    <App name = "react" favoriteNumber={1}>리액트</App>,
   document.getElementById('root')
 );
 
