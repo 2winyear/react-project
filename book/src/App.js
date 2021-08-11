37baf96de8760574f87541ff08cd2db94c431866
seungyel <seungyel@seungyelui-MacBookPro.local>
2021-08-11 11:57:30 +0900

    book ~5

--- book/src/App.js
+++ book/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+import Counter from './Counter';
+import Say from './Say';
+import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   static defaultProps ={
@@ -15,6 +20,14 @@ class App extends Component {
   const { name, favoriteNumber, children } = this.props; 
   return( 
     <div>
+      <ValidationSample />
+      <ScrollBox ref = {(ref) => this.scrollBox=ref}/>
+      <button onClick={() => this.scrollBox.scrollToBottom()}>
+        맨 밑으로
+      </button>
+      <Counter/>
+      <Say />
+      <EventPractice />
       안녕하세요 {name}
       제가 좋아하는 숫자는 {favoriteNumber}
       {children}
