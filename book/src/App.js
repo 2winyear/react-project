7fc0a161b10f2b9f59f1c7eac4bbd7650f042865
2winyear <lsy2246@naver.com>
2021-08-14 01:33:05 +0900

    chap 8 reading

--- book/src/App.js
+++ book/src/App.js
@@ -1,59 +1,27 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
 import LifeCycleSample from './LifeCycleSample';
+import Info from './Info';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  static defaultProps ={
-    name: '기본이름'
-  };
-
-  state = {
-    color: '#000000'
-  }
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  }
-  
-  static propTypes = {
-    name: PropTypes.string,
-    favoriteNumber: PropTypes.number.isRequired
-  }
-  
-  render(){
-  const { name, favoriteNumber, children } = this.props; 
+const App = () => {
+  const [visible, setVisible] = useState(false);
   return( 
     <div>
-      <button onclick={this.handleClick}>랜덤 색상</button>
-      <LifeCycleSample color={this.state.color}/>
+      <button onClick={()=>{setVisible(!visible);}}>{visible? '숨기기': '보이기'}</button><hr />
+      {visible && <Info />}
+      <LifeCycleSample/>
       <IterationSample />
       <ValidationSample />
-      <ScrollBox ref = {(ref) => this.scrollBox=ref}/>
-      <button onClick={() => this.scrollBox.scrollToBottom()}>
-        맨 밑으로
-      </button>
       <Counter/>
       <Say />
       <EventPractice />
-      안녕하세요 {name}
-      제가 좋아하는 숫자는 {favoriteNumber}
-      {children}
     </div>
     );
-  }
 }
 
 
--- book/src/index.js
+++ book/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 ReactDOM.render(
-    <App name = "react" favoriteNumber={1}>리액트</App>,
+    <App/>,
   document.getElementById('root')
 );
 
